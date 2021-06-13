870a82b165ec7d48da82935d2b1e5314d43b2dfa
Rakesh Rankawat <rakesh.rankawat@outlook.com>
2021-06-14 00:23:25 +0530

    Head Tags & Layout

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,13 +1,9 @@
-import { useRouter } from 'next/router';
+import Layout from '../../components/Layout';
 
 export default function EventPage() {
-  const router = useRouter();
-
   return (
-    <div>
-      <h1>Single Event</h1>
-      <p>{router.query.slug}</p>
-      <button onClick={() => router.push('/')}>Home</button>
-    </div>
+    <Layout>
+      <h1>Edit Event</h1>
+    </Layout>
   );
 }
