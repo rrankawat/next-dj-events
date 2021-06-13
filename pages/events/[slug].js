34ae459b28dd9ec0de4f3a8e9a4e1356f43938cf
Rakesh Rankawat <rakesh.rankawat@outlook.com>
2021-06-13 22:33:17 +0530

    Next Link & Router

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,7 +1,13 @@
+import { useRouter } from 'next/router';
+
 export default function EventPage() {
+  const router = useRouter();
+
   return (
     <div>
       <h1>Single Event</h1>
+      <p>{router.query.slug}</p>
+      <button onClick={() => router.push('/')}>Home</button>
     </div>
   );
 }
