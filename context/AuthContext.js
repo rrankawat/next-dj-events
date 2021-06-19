39cd56db13792704fc455674ef61fb7443010228
Rakesh Rankawat <rakesh.rankawat@outlook.com>
2021-06-19 14:59:23 +0530

    Production URL

--- context/AuthContext.js
+++ context/AuthContext.js
@@ -68,7 +68,7 @@ export const AuthProvider = ({ children }) => {
   };
 
   // Check if user is logged in
-  const checkUserLoggedIn = async (user) => {
+  const checkUserLoggedIn = async () => {
     const res = await fetch(`${NEXT_URL}/api/user`);
     const data = await res.json();
 
